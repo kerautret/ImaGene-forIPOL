84b7836d54f4370c027c9b198d82d018fec2bbc6
Kerautret <kerautre@loria.fr>
2013-09-16 10:13:53 +0200

    Adding Otsu in pgm2freeman

--- bin/pgm2freeman.cxx
+++ bin/pgm2freeman.cxx
@@ -29,6 +29,142 @@ using namespace ImaGene;
 
 static Arguments args;
 
+
+/*
+ * Return the histogram associated to the 2D image 
+ */
+std::vector<unsigned int>
+importHistoFromPGM
+( std::istream & in, 
+  KnSpace* & ks,
+  KnCharSet* & voxset,
+  uint threshold, uint bordXY, unsigned int &imageSize )
+{
+  bool quiet=true;
+  ks = 0;
+  voxset = 0;
+  string str;
+  getline( in, str );
+  if ( ! in.good() ) return std::vector<unsigned int>();
+  if ( str != "P5" ) return std::vector<unsigned int>();
+  do
+    {
+      getline( in, str );
+      if ( ! in.good() ) return std::vector<unsigned int>();
+    }
+  while ( str[ 0 ] == '#' || str=="");
+  istringstream str_in( str );
+  Kn_size sizes[ 2 ];
+  str_in >> sizes[ 0 ] >> sizes[ 1 ];
+  uint xMaxObj=sizes[0];
+  uint yMaxObj=sizes[1];
+
+  imageSize = sizes[0]*sizes[1];
+  sizes[0]+=2*bordXY;
+  sizes[1]+=2*bordXY;
+  
+  
+  getline( in, str );
+  istringstream str2_in( str );
+  int max_value;
+  str2_in >> max_value;
+  std::vector<unsigned int> histo(max_value+1, 0);
+  if ( ! in.good() ) return std::vector<unsigned int>();
+  if(!quiet){
+    cerr << "# PGM " << sizes[ 0 ] << " " << sizes[ 1 ] 
+	 << " " << max_value << " from <" << str << ">" << endl;
+  }
+  ks = new KnSpace( 2, sizes );
+  if ( ks == 0 ) return std::vector<unsigned int>();
+  voxset = new KnCharSet( KnCharSet::ucreate( *ks, 
+					      ks->uspel( ks->ufirst() ),
+					      0 ) );
+  if ( voxset == 0 )
+    {
+      delete ks;
+      ks = 0;
+      return std::vector<unsigned int>();
+    }
+	
+  KnSpaceScanner scan2( *ks, 
+ 			ks->ufirstCell( ks->dim() ),
+			ks->ulastCell( ks->dim() ) );
+  uint nb_read = 0;
+  Kn_uid last_y, last_x;
+  Kn_uid p = scan2.begin();
+  uint x,y;
+  
+  in >> noskipws;
+  for ( y=0, last_y = scan2.last( p, 1 );
+	p <= last_y; y++,
+	p += scan2.gotonext( 1 ) )
+    {
+      for ( x=0, last_x = scan2.last( p, 0 ); 
+	    p <= last_x; x++,
+	      p++ ) // NB: 'scan.gotonext( 0 )' == 1;
+	{ //... whatever
+	  if(!((x<bordXY) ||( y<bordXY)  || 
+	       (x>=(xMaxObj+bordXY))|| (y>=(yMaxObj+bordXY)) )){
+	    unsigned char c; 
+	    in >> c;
+	    histo[c]++;
+	    if ( in.good() ) ++nb_read;
+	    if ( ( (uint) c ) < threshold )
+	      (*voxset)[ p ] = true; 
+	    
+	  }
+	}
+    }
+  if ( in.fail() || in.bad() )
+    {
+      cerr << "# nbread=" << nb_read << endl;
+      delete voxset;
+      delete ks;
+      voxset = 0;
+      ks = 0;
+      return std::vector<unsigned int>();
+    }
+  in >> skipws;
+  return histo;
+}
+
+unsigned int 
+getThreshold(std::vector<unsigned int> histo, unsigned int imageSize){
+  unsigned int sumA = 0;
+  unsigned int sumB = imageSize;
+  int muA=0;
+  int muB=0;
+  for( unsigned int t=0; t< histo.size();t++){
+    muB+=histo[t]*t;
+  }
+  
+  unsigned int thresholdRes=0;
+  double valMax=0.0;
+  for( unsigned int t=0; t< histo.size(); t++){
+    sumA+=histo[t];
+    if(sumA==0)
+      continue; 
+    sumB-=histo[t];
+    muA+=histo[t]*t;
+    muB-=histo[t]*t;
+    if(sumB==0){
+      break;
+    }
+    double muAr=muA/(double)sumA;
+    double muBr=muB/(double)sumB;
+    double sigma=  (double)sumA*(double)sumB*(muAr-muBr)*(muAr-muBr);
+    if(valMax<sigma){
+      valMax=sigma;
+      thresholdRes=t;
+    }
+
+  }
+
+  
+  return thresholdRes;
+}
+
+
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -42,18 +178,19 @@ main( int argc, char** argv )
 {
   // -------------------------------------------------------------------------
   // Prepare arguments.
+
   args.addOption( "-threshold", "-threshold <val>: threshold value for binarizing PGM gray values (def. is 128).", "128" );
   args.addOption( "-min_size", "-min_size <m>: minimum digital length of contours for output (def. is 4).", "4" );
   args.addOption( "-badj", "-badj <0/1>: 0 is interior bel adjacency, 1 is exterior (def. is 0).", "0" );
   args.addOption("-selectContour", "-selectContour <x0> <y0> <distanceMax>: select the contours for which the first point is near (x0, y0) with a distance less than <distanceMax>","0", "0", "0" );
   args.addBooleanOption("-invertVerticalAxis", "-invertVerticalAxis used to transform the contour representation (need for DGtal), used only for the contour displayed, not for the contour selection (-selectContour). ");
-  
+
   
   if ( ( argc <= 0 ) 
        || ! args.readArguments( argc, argv ) ) 
     {
       cerr << args.usage( "pgm2freeman", 
-			  "Extracts all 2D contours from a PGM image given on the standard input and writes them on the standard output as FreemanChain's.",
+			  "Extracts all 2D contours from a PGM image given on the standard input and writes them on the standard output as FreemanChain's. By default the threshold is computed with the Otsu algorithm but you can also set it manually (using -threshold)",
 			  "" )
 	   << endl;
       return 1;
@@ -63,12 +200,18 @@ main( int argc, char** argv )
   KnSpace* ks;
   KnCharSet* voxset;
   uint threshold = (uint) args.getOption( "-threshold" )->getIntValue( 0 );
-  if ( ! ShapeHelper::importFromPGM( cin, ks, voxset, threshold, 1 ) )
+  uint imageSize = 0;
+  std::vector<unsigned int> histo= importHistoFromPGM( cin, ks, voxset, threshold, 1, imageSize ); 
+  if(!args.check("-threshold")){
+    threshold = getThreshold(histo, imageSize);
+    cerr << "Automatic threshold with otsu=" <<  threshold << endl;
+  }
+  if ( histo.size()==0 )
     {
       cerr << "Error reading PGM file." << endl;
       return 2;
     }
-  
+
   Vector2i ptReference;
   double distanceMax=0.0;
   //Rajout (BK) 
